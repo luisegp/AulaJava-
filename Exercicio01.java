1b5846a12f51d228a44327017f5f785f93a5a48c
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-11 21:37:24 -0300

    v2

--- Exercicio01.java
+++ Exercicio01.java
@@ -4,7 +4,15 @@ public class Exercicio01 {
     public static void executar(){
         Scanner leitor = new Scanner(System.in);
         
+        System.out.println("Informe um número: ");
+        float num1 = leitor.nextFloat();
 
+        if(num1 > 10){
+            System.out.println("O número informado é maior que 10");
+        }
+        else{
+            System.out.println("O número informado é menor ou igual a 10.");
+        }
 
 
         leitor.close();
