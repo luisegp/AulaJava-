f66de19dfb53c455e1400b6d62c4d6bf42e6a4c0
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-11 21:28:13 -0300

    Primeiro commit no branch main

---
+++ Exercicio01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void executar(){
+        Scanner leitor = new Scanner(System.in);
+        
+
+
+
+        leitor.close();
+    }
+}
---
+++ Programa.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+
+public class Programa {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite qual exercício deseja executar: ");
+
+        
+
+        leitor.close();
+    }
+}
