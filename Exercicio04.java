2d57b94c1a9cbcfda248f02e0af0ad182c46d09f
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-18 20:38:20 -0300

    V1

---
+++ Exercicio04.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Exercicio04 {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+
+        leitor.close();
+    }
+}
