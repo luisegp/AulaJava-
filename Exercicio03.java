120c1852f96cb2089cd7f670d470e2f471d8e262
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-18 20:11:19 -0300

    V2

--- Exercicio03.java
+++ Exercicio03.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 public class Exercicio03 {
     public static void main(String[] args){
         Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe um número: ");
+        int num1 = leitor.nextInt();
+
+        System.out.println("Informe mais um número");
+        int num2 = leitor.nextInt();
+
+        if (num1 == num2) {
+            System.out.println("Os números informados são iguais.");
+        }else if(num1 > num2){
+            System.out.println("O primeiro número informado é maior que o segundo");
+        }else{
+            System.out.println("O segundo número é maior");
+        }
 
         leitor.close();
     }
