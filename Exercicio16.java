8a6bb53a4792ba43f540feb65fa906f926a79196
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-19 20:56:32 -0300

    v1

--- Exercicio16.java
+++ Exercicio16.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Exercicio16 {
-    
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite a nota 1");
+        double n1 = leitor.nextDouble();
+
+        System.out.println("Digite a nota 2");
+        double n2 = leitor.nextDouble();
+
+        System.out.println("Digite a nota 3");
+        double n3 = leitor.nextDouble();
+
+        double media = (n1 + n2 + n3)/3;
+
+        System.out.println("A média dos valores dados é: " + media);
+        
+        leitor.close();
+    }
 }
