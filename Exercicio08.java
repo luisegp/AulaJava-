4dd63e3d0fce3d2e2963208e8fdc3faeef75b409
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-18 21:29:38 -0300

    v1

---
+++ Exercicio08.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Exercicio08 {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe um número: ");
+        int num = leitor.nextInt();
+
+        if (num >= 50) {
+            System.out.println("O número informado é maior ou igual a 50");
+        }else{
+            System.out.println("O número informado é menor que 50");
+        }
+
+        leitor.close();
+    }
+}
