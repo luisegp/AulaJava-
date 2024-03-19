8a790d798fa8e5c05567e10c274381d0b873144f
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-18 21:02:40 -0300

    V2

--- Exercicio04.java
+++ Exercicio04.java
@@ -3,6 +3,17 @@ public class Exercicio04 {
     public static void main(String[] args){
         Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Informe primeiro número: ");
+        int num1 = leitor.nextInt();
+
+        System.out.println("Informe o segundo número");
+        int num2 = leitor.nextInt();
+
+        int mult = num1 * num2, soma = num1 + num2, sub = num1 - num2;
+        float div = num1 / num2;
+
+        System.out.println("O resultado das operações com os números fornecidos é: Multiplicação: " + mult + 
+        "Divisão: " + div + "Soma: " + soma + "Subtração: " + sub);
 
         leitor.close();
     }
