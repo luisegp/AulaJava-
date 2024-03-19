8030dce9e27dda3a3e4968638f3b3fd3657d9189
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-18 21:38:28 -0300

    v1

---
+++ Exercicio10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Exercicio10 {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe um número entre 1 e 5: ");
+        int num = leitor.nextInt();
+
+        if (num > 5 && num < 0) {
+            System.out.println("O número informado está inválido");
+        }else{
+            if (num == 1){
+                System.out.println("Um");
+            }else if (num == 2){
+                System.out.println("Dois");
+            }else if (num == 3){
+                System.out.println("Três");
+            }else if (num == 4){
+                System.out.println("Quatro");
+            }else if (num == 5){
+                System.out.println("Cinco");
+            }
+        }
+
+        leitor.close();
+    }
+}
