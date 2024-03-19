1c8adf7eaa78abdc1fdfab3726e8232d77ed88e5
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-19 20:50:51 -0300

    v1

--- Exercicio15.java
+++ Exercicio15.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class Exercicio15 {
-    
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite as coordenadas de p1 (X1)");
+        double x1 = leitor.nextDouble();
+
+        System.out.println("Digite as coordenadas de p1 (Y1)");
+        double y1 = leitor.nextDouble();
+
+        System.out.println("Digite as coordenadas de p2 (X2)");
+        double x2 = leitor.nextDouble();
+
+        System.out.println("Digite as coordenadas de p2 (Y2)");
+        double y2 = leitor.nextDouble();
+
+        double distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2-y1, 2));
+
+        System.out.println("Distância entre dois pontos: " + distancia);
+        
+        leitor.close();
+    }
 }
