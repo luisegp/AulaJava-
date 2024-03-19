33c3f35d925ac232d37df9f344ba4f1c05d998c2
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-19 19:36:52 -0300

    v2

--- Exercicio14.java
+++ Exercicio14.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Exercicio14 {
-    
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Vamos fazer uma PA. Informe o valor de do A1: ");
+        int a1 = leitor.nextInt();
+
+        System.out.println("Infome o valor de N: ");
+        int n = leitor.nextInt();
+
+        System.err.println("Informe o valor de R: ");
+        int r = leitor.nextInt();
+
+        int pa = a1+ (n-1)*r;
+
+        System.out.println("O valor de An da progressão é: " + pa);
+
+        leitor.close();
+    }
 }
