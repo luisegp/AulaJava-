959e02da7df58c7f9424f46d54534985e370862c
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-18 21:24:24 -0300

    v1

---
+++ Exercicio06.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Exercicio06 {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe a temperatura em graus celsius: ");
+        int celsius = leitor.nextInt();
+        float fahr = (9*celsius+160)/5;
+
+        System.out.println("A quantidade de graus celsius inserido é " + fahr + "graus fahrenheit");
+
+        leitor.close();
+    }
+}
