ba8000acfa532267debbbebdbc40f89acbdf057b
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-19 21:00:45 -0300

    v1

--- Exercicio17.java
+++ Exercicio17.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
 public class Exercicio17 {
-    
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite a nota 1");
+        double n1 = leitor.nextDouble();
+
+        System.out.println("Digite o peso 1");
+        double p1 = leitor.nextDouble();
+
+        System.out.println("Digite a nota 2");
+        double n2 = leitor.nextDouble();
+
+        System.out.println("Digite o peso 2");
+        double p2 = leitor.nextDouble();
+
+        System.out.println("Digite a nota 3");
+        double n3 = leitor.nextDouble();
+        
+        System.out.println("Digite o peso 3");
+        double p3 = leitor.nextDouble();
+
+        double media = (n1 * p1 + n2 * p2 + n3 * p3) / p1 * p2 * p3;
+
+        System.out.println("A média dos valores dados é: " + media);
+        
+        leitor.close();
+    }
 }
