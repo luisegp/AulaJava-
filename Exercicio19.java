dcd1d4c06ce67dfd9e001aa7996bfb7693d1cbc0
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-19 21:08:21 -0300

    v2

--- Exercicio19.java
+++ Exercicio19.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class Exercicio19 {
-    
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o raio: ");
+        double raio = leitor.nextDouble();
+
+        System.out.println("Digite a altura");
+        double altura = leitor.nextDouble();
+
+        double vol = 3.14*raio*raio*altura;
+
+        System.out.println("O volume do cilindro é: " + vol);
+        
+        leitor.close();
+    }
 }
