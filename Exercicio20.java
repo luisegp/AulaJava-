b768b07fae99c9ec0939852fec39e896c164764e
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-19 21:17:37 -0300

    v1

--- Exercicio20.java
+++ Exercicio20.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Exercicio20 {
-    
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite a quantidade de km/l seu carro faz");
+        double n1 = leitor.nextDouble();
+
+        System.out.println("Digite a quantidade de horas da viagem");
+        double n2 = leitor.nextDouble();
+
+        System.out.println("Digite a velocidade média da viagem");
+        double n3 = leitor.nextDouble();
+
+        double quant = (n2 * n3) / n1;
+
+        System.out.println("A média dos valores dados é: " + quant);
+        
+        leitor.close();
+    }
 }
