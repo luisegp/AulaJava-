86c5d636e7b4938b991dda55f358af5a6ef97227
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-22 21:16:06 -0300

    v2

--- Exercicio12.java
+++ Exercicio12.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Exercicio12 {
-    public static void main(String[] args) {
+    public static void executar() {
         Scanner leitor = new Scanner(System.in);
 
         System.out.print("Digite o número do mês (1 a 12): ");
