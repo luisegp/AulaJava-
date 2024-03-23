f9236725ff19e293388b0ca9bcc82321593b94bd
Luis Eduardo <luiseduardo.ilha@gmail.com>
2024-03-22 21:14:39 -0300

    v2

--- Exercicio09.java
+++ Exercicio09.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Exercicio09 {
-    public static void main(String[] args){
+    public static void executar(){
         Scanner leitor = new Scanner(System.in);
         System.out.println("Informe um número: ");
         int num = leitor.nextInt();
